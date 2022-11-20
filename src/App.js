6560160cf47ed38a90acc6464bd488383fbd2564
unknown <vaga5535@mailr.ru>
2022-11-20 13:36:17 +0300

    20.11.2022gg 13:36

--- src/App.js
+++ src/App.js
@@ -6,27 +6,25 @@ function App() {
   return (
     <BrowserRouter>
       <div className='body'>
-        <img className='body_img'/>
         <div className='header'>
           <div className='header_1'>
+            <img className='body_img' />
             <div className='body_logo          body_header_1_item'></div>
             <div className='body_header_1_line body_header_1_item'></div>
             <div className='body_logoname      body_header_1_item'>ENGINEER</div>
           </div>
           <div className='header_2'>
-            <a href='/Main' className='body_header_2_item1          body_h_2_item'>Главная</a>
-            <a href='/Projects' className='body_header_2_item2      body_h_2_item'>Проекты</a>
-            <a href='/AboutPlatform' className='body_header_2_item3 body_h_2_item'>О Платформе</a>
+            <a href='/Main' className='body_h_2_item'>Главная</a>
+            <a href='/Projects' className='body_h_2_item'>Проекты</a>
+            <a href='/AboutPlatform' className='body_h_2_item'>О Платформе</a>
           </div>
         </div>
 
-        <div>
-          <Routes>
-            <Route path='/Main'          element={<Main />} />
-            <Route path='/Projects'      element={<Projects />} />
-            <Route path='/AboutPlatform' element={<Main />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path='/Main' element={<Main />} />
+          <Route path='/Projects' element={<Projects />} />
+          <Route path='/AboutPlatform' element={<Main />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
